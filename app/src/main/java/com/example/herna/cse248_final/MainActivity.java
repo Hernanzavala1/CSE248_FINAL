6e66de5e3e012d3a0c985dfb490947d6ed691b3f
Hernan Zavala <hernanjosuezavala@hotmail.com>
2018-09-19 20:42:11 -0500

    Added registration activity

--- app/src/main/java/com/example/herna/cse248_final/MainActivity.java
+++ app/src/main/java/com/example/herna/cse248_final/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.herna.cse248_final;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+    private Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        registerButton = (Button)(findViewById(R.id.registerButton));
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setUpregisterButton();
+            }
+        });
+    }
+    public void setUpregisterButton(){
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/herna/cse248_final/RegistrationActivity.java
@@ -0,0 +1,12 @@
+package com.example.herna.cse248_final;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegistrationActivity extends Activity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_activity);
+    }
+}
