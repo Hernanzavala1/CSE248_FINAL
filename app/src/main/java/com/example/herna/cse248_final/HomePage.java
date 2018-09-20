8db915c88702314c417f59028f48f15dc287b348
Hernan Zavala <hernanjosuezavala@hotmail.com>
2018-09-20 14:22:25 -0500

    Added a home Page Activity

---
+++ app/src/main/java/com/example/herna/cse248_final/HomePage.java
@@ -0,0 +1,14 @@
+package com.example.herna.cse248_final;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+public class HomePage extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+}
