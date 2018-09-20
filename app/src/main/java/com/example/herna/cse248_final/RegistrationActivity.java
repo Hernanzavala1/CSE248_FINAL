47c3bd396ffbe1447238b5dbc1afb5d6fa6f3db1
Hernan Zavala <hernanjosuezavala@hotmail.com>
2018-09-20 00:23:31 -0500

    updated the register page

--- app/src/main/java/com/example/herna/cse248_final/RegistrationActivity.java
+++ app/src/main/java/com/example/herna/cse248_final/RegistrationActivity.java
@@ -1,12 +1,26 @@
 package com.example.herna.cse248_final;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistrationActivity extends Activity {
+    private Button cancelButton;
+    private Button signUpButton;
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_activity);
+        cancelButton = (Button)findViewById(R.id.Cancel_Btn);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegistrationActivity.this,MainActivity.class );
+                startActivity(intent);
+
+            }
+        });
     }
 }
